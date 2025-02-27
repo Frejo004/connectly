3445622bbbd656cfd592efee87305ce58ed5dffe
Frejus DASSI <frejohope@gmail.com>
2025-02-27 17:58:42 +0100

    Ajout du composant AppFooter avec une structure améliorée et des sections pour les services, ressources, produits et packages, ainsi qu'un formulaire d'inscription à la newsletter.

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,3 +1,81 @@
+<script setup lang="ts">
+const columns = [
+  {
+    label: "Service",
+    children: [
+      { label: "Assitance" },
+      { label: "Documentation" },
+      { label: "Forum" },
+      { label: "Contact" },
+    ],
+  },
+  {
+    label: "Ressources",
+    children: [
+      { label: "Starter Kits" },
+      { label: "Release Notes" },
+      { label: "Blog" },
+      { label: "Partners" },
+      { label: "News" },
+      { label: "Jobs" },
+      { label: "Careers" },
+    ],
+  },
+  {
+    label: "Products",
+    children: [
+      { label: "Cloud" },
+      { label: "Forge" },
+      { label: "Nightwatch" },
+      { label: "Nova" },
+    ],
+  },
+  {
+    label: "Packages",
+    children: [
+      { label: "Cashier" },
+      { label: "Dusk" },
+      { label: "Horizon" },
+      { label: "Octane" },
+      { label: "Scout" },
+      { label: "Pennant" },
+      { label: "Pint" },
+    ],
+  },
+];
+</script>
+
 <template>
-  <footer>Footer</footer>
+  <UFooter>
+    <template #top>
+        <UContainer>
+            <UFooterColumn :links="links">
+                <template #right>
+                    <form>
+                        <UFormFiel name="email" label="S'incrire à notre newsletter" size="lg" />
+                        <template #right >
+                            <UButton type="submit" size="xs" color="orange" label="S'incrire" />
+                        </template>
+                    </form>
+                </template >
+            </UFooterColumn>
+        </UContainer>
+    </template>
+
+    <template #left >
+        <p class="text-sm">
+            Copyright &copy; {{ new Date().getFullYear() }}. Tous droits réservés.
+        </p>
+    </template>
+
+    <template #right >
+        <UColorModeButton/>
+    </template>
+
+    <template #center >
+        <UButon to="https://github" target="_blank" label="Visiter Mon GitHub"/>
+    </template>
+
+
+  </UFooter>
 </template>
