5c5ca3183377434df1a549830592768dd76e24de
Frejus DASSI <frejohope@gmail.com>
2025-02-27 16:32:11 +0100

    Ajout de la configuration de base pour une application Nuxt, y compris les fichiers de configuration, les composants de l'interface utilisateur et le fichier README.

---
+++ components/AppHeader.vue
@@ -0,0 +1,24 @@
+<script> 
+    const items = [
+        {
+            label: 'Blog',
+            to:'/blog',
+        },
+
+        {
+            label: 'About',
+            to:'/about',
+        }
+    ]
+
+</script>
+
+
+
+
+<template>
+    <header>
+        Header
+        
+    </header>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <main>
+            <slot/>
+        </main>
+    </div>
+</template>
+
+
+<script lang="ts" script></script>
+
+<style ></style>
