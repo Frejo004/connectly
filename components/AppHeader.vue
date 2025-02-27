97b1adacf5af860b999ee4c7b4b641ec136adf30
Frejus DASSI <frejohope@gmail.com>
2025-02-27 17:01:36 +0100

    Refactor le composant AppHeader pour utiliser la syntaxe de script setup et améliorer la structure du code

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,24 +1,30 @@
-<script> 
-    const items = [
-        {
-            label: 'Blog',
-            to:'/blog',
-        },
-
-        {
-            label: 'About',
-            to:'/about',
-        }
-    ]
-
+<script setup lang="ts">
+const links = [
+  {
+    label:'Blog',
+    to:'/blog',
+  },
+  {
+    label:'A propos',
+    to:'/about',
+  }
+];
 </script>
 
-
-
-
 <template>
-    <header>
-        Header
-        
-    </header>
+  <UHeader>
+    <template #left>
+      <NuxtLink to="/">
+        <img src="/logo.png" alt="Loge de Connectly" class="w-auto h-6 shrink-0">
+      </NuxtLink>
+    </template>
+
+    <UNavigationLinks :links="links" />
+
+    <template #right>
+        <UButton label="Se connecter" variant="solid" color= "orange" to"/login" />
+    </template>
+  </UHeader>
 </template>
+
+<style scoped></style>
