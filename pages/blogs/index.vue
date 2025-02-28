01e725835051c5665ab982d3bd937f7166ffe8f0
Frejus DASSI <frejohope@gmail.com>
2025-02-28 19:30:33 +0100

    Ajout de la page de blogs avec récupération dynamique des articles et affichage dans une liste

---
+++ pages/blogs/index.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+const {data: posts} = await useAsyncData('posts',  () => queryCollection('blog').all());
+</script>
+
+
+<template>
+    <UContainer>
+        <UBlogList>
+            <UBlogPost v-for ="post in posts"
+            :key="post.id" 
+            :title="post.title" 
+            :description="post.description" 
+            :image="post.image" 
+            :authors="post.auteurs"
+            :date="formatDate(post.date)"
+            :badge="{...post.badge,  variant: 'subtle'}">
+            :to="post.path"
+            </UBlogPost>
+        </UBlogList>
+    </UContainer>
+
+</template>
+
+
+<style scoped>
+
+
+</style>
