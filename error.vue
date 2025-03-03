0092f28250dc0fb9a593cc2a6a4dc4a07ae6ab61
Frejus DASSI <frejohope@gmail.com>
2025-03-03 18:28:40 +0100

    Ajout d'un composant d'erreur personnalisé pour la gestion des erreurs dans l'application

---
+++ error.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+    import type { NuxtError } from '#app';
+    const   props = defineProps({
+        error: Object as () => NuxtError })
+
+</script>
+
+<template>
+    <div>
+        <AppHeader />
+
+        <UMain>
+            <UPage>
+                <UPageError :error="error"/>
+            </UPage>
+        </UMain>
+
+        <AppFooter />
+    </div>
+</template>
+
+<style scoped></style>
