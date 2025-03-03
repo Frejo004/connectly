2ef6794dc24bd3a19ea65d4389f1fb8a6e1ef277
Frejus DASSI <frejohope@gmail.com>
2025-03-03 18:28:29 +0100

    Ajout de la configuration de la collection de données avec un schéma détaillé pour la gestion des sections et des fonctionnalités

--- content.config.ts
+++ content.config.ts
@@ -25,6 +25,74 @@ export default defineContentConfig({
                     label:z.string(),
                 })
            })
-       })
-   }
+       }),
+
+       data: defineCollection({
+        type: 'data',
+        source: '0.index.yml',
+        schema: z.object({
+                title: z.string(),
+                description: z.string(),
+                navigation: z.boolean(),
+                hero: z.object({
+                    title: z.string(),
+                    description: z.string(),
+                    links: z.array(
+                        z.object({
+                            label: z.string(),
+                            icon: z.string(),
+                            trailing: z.boolean().optional(),
+                            to: z.string(),
+                            target: z.string(),
+                            size: z.string().optional(),
+                            color: z.string().optional(),
+                            variant: z.string().optional(),
+                        })
+                    )
+                }),
+                sections: z.array(
+                    z.object({
+                        title: z.string(),
+                        description: z.string(),
+                        id: z.string().optional(),
+                        orientation: z.string(),
+                        reverse: z.boolean().optional(),
+                        features: z.array(
+                            z.object({
+                                name: z.string(),
+                                description: z.string(),
+                                icon: z.string(),
+                            })
+                        )
+                    })
+                ),
+                features: z.object({
+                    title: z.string(),
+                    description: z.string(),
+                    items: z.array(
+                        z.object({
+                            title: z.string(),
+                            description: z.string(),
+                            icon: z.string(),
+                        })
+                    )
+                }),
+                cta: z.object({
+                    title: z.string(),
+                    description: z.string(),
+                    links: z.array(
+                        z.object({
+                            label: z.string(),
+                            to: z.string(),
+                            target: z.string(),
+                            icon: z.string().optional(),
+                            trailingIcon: z.string().optional(),
+                            variant: z.string().optional(),
+                        })
+                    )
+                })
+            }),
+            
+        })
+   },
 })
